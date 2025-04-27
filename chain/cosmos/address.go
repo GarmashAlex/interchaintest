828c26d845977abc0a8ce54b75e3208f5cb6caea
GarmashAlex <garmasholeksii@gmail.com>
2025-04-27 18:03:41 +0300

    refactor: update CosmosChain to use common address converter

--- chain/cosmos/address.go
+++ chain/cosmos/address.go
@@ -1,33 +1,21 @@
 package cosmos
 
 import (
-	"errors"
-	"strings"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/bech32"
+
+	"github.com/strangelove-ventures/interchaintest/v8/ibc"
 )
 
 // AccAddressFromBech32 creates an AccAddress from a Bech32 string.
 // https://github.com/cosmos/cosmos-sdk/blob/v0.50.2/types/address.go#L193-L212
 func (c *CosmosChain) AccAddressFromBech32(address string) (addr sdk.AccAddress, err error) {
-	if len(strings.TrimSpace(address)) == 0 {
-		return sdk.AccAddress{}, errors.New("empty address string is not allowed")
-	}
-
-	bz, err := sdk.GetFromBech32(address, c.Config().Bech32Prefix)
-	if err != nil {
-		return nil, err
-	}
-
-	err = sdk.VerifyAddressFormat(bz)
-	if err != nil {
-		return nil, err
-	}
-
-	return sdk.AccAddress(bz), nil
+	// Use the common implementation
+	converter := ibc.NewCommonAddressBech32Converter(c.Config())
+	return converter.AccAddressFromBech32(address)
 }
 
 func (c *CosmosChain) AccAddressToBech32(addr sdk.AccAddress) (string, error) {
-	return bech32.ConvertAndEncode(c.Config().Bech32Prefix, addr)
+	// Use the common implementation
+	converter := ibc.NewCommonAddressBech32Converter(c.Config())
+	return converter.AccAddressToBech32(addr)
 }
